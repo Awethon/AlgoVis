b66b9e11653b460ffad335a4a47a5f05c2f4f191
Awethon <gorodgorod63@gmail.com>
2017-06-22 18:46:22 +0300

    Initial commit

---
+++ src/main/java/ControlBarController.java
@@ -0,0 +1,11 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ControlBarController {
+    @FXML
+    private TextField tf;
+
+    public TextField getTextField() {
+        return tf;
+    }
+}
---
+++ src/main/java/Controller.java
@@ -0,0 +1,36 @@
+import javafx.fxml.FXML;
+import javafx.scene.chart.XYChart;
+import org.controlsfx.control.Notifications;
+
+import java.util.Objects;
+
+public class Controller {
+    @FXML
+    private ArrayBarChartController arrayBarChartController;
+    @FXML
+    private ControlBarController controlBarController;
+
+
+    @FXML
+    public void initialize() {
+        controlBarController.getTextField().textProperty().addListener((observable, oldValue, newValue) -> {
+            if (Objects.equals(newValue, "")) return;
+            if (newValue.matches("\\d+$")) {
+                Integer n = Integer.parseInt(newValue);
+                if (n >= 0 && n <= 250) {
+                    arrayBarChartController.clear();
+                    arrayBarChartController.getBC().setCategoryGap(150.0 / n);
+                    arrayBarChartController.getBC().layout();
+                    for (int i = 0; i < n; i++) {
+                        arrayBarChartController.add(new XYChart.Data(Integer.toString(i), i + 1));
+                    }
+                } else {
+                    Notifications.create().title("Length error").text("Array size must be 1-250").showError();
+                }
+            } else {
+                Notifications.create().title("Input data error").text("Field must be filled with number from 1 to 250").showError();
+            }
+        });
+    }
+
+}
