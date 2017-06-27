66f3828e62d59f76278b8916fc25df928062da97
sh-ad <alex1097@mail.ru>
2017-06-27 15:02:10 +0300

    add array generate

--- src/main/Models/SequenceGenerator.java
+++ src/main/Models/SequenceGenerator.java
@@ -6,6 +6,8 @@ public class SequenceGenerator implements ISequenceGenerator {
     public int[] generate(String generationMode, int size) {
         if(generationMode.equals("Random"))
             return generateRandom(size);
+        else if(generationMode.equals("Almost sorted"))
+            return generateAlmostSorted(size);
         else
             return generateReverse(size);
     }
@@ -15,7 +17,7 @@ public class SequenceGenerator implements ISequenceGenerator {
         Random random = new Random();
         int[] result = new int[size];
         for (int i = 0; i < size; i++)
-            result[i] = 15 + random.nextInt(100);
+            result[i] = random.nextInt(size);
         return result;
     }
 
@@ -24,6 +26,18 @@ public class SequenceGenerator implements ISequenceGenerator {
         int[] result = new int[size];
         for (int i = 0; i < size; i++)
             result[i] = size - i + 1;
+        return result;
+    }
+
+    private int[] generateAlmostSorted(int size)
+    {
+        Random random = new Random();
+        int[] result = new int[size];
+        for (int i = 0; i < size-1; i++)
+            result[i] = i;
+
+        result[random.nextInt(size)] = random.nextInt(size);
+
         return result;
     }
 }
