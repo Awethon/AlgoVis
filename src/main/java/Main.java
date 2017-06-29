e8c07f42150d2f63de4a75e9768e6d94114ea184
sh-ad <alex1097@mail.ru>
2017-06-29 19:30:21 +0300

    add helpView.fxml
    Добавлен файл с информацией о сортировке

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/rootView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/helpView.fxml"));
+
         primaryStage.setTitle("AlgoVis [Merge Sort]");
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.setMinWidth(600);
